cafdfabd7409bffd052f263d90962b4d576d818e
Kristof Galla <gallakristof@gmail.com>
2016-10-07 00:28:48 +0200

    Save new board to local storage

--- static/base.js
+++ static/base.js
@@ -1,13 +1,50 @@
 $(document).ready(function() {
     $("#boards").hide();
     $("#edit").hide();
+
     $("#plus").click(function() {
-        $("#add").replaceWith($("#edit"));
+        $("#add").hide();
         $("#edit").fadeIn("slow");
     });
+
+    $("#cancel").click(function() {
+        $("#edit").hide();
+        $("#add").fadeIn("slow");
+    });
+
     $("#save").click(function() {
-        $("#edit").replaceWith($("#boards"));
-        $("#boards").fadeIn("slow");
-        $("#add").fadeIn("fast");
+        $("#edit").hide();
+        $("#add").fadeIn("slow");
+        makeNewBoard();
     });
 });
+
+var makeNewBoard = function() {
+    // get tile from form (test if filled) and generate code
+    var title = document.getElementById("title").value;
+    if (title === "" || title === null) {
+        return false;
+    }
+    // make a new board object
+    var newBoard = {
+        boardId: "asdf",
+        title: title
+    }
+    // append board object to local storage
+    saveBoard(newBoard);
+    // prepare div
+
+    // prepend div of the new board
+    // clear form value
+    document.getElementById("title").value = ""
+}
+
+var saveBoard = function(boardObj) {
+    var x = localStorage.getItem('boards');
+    if (x === null) {
+        x = JSON.stringify([]);
+    }
+    x = JSON.parse(x);
+    x.push(boardObj);
+    localStorage.setItem('boards', JSON.stringify(x));
+}
