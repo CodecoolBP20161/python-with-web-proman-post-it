18aca151ffadb9f82f868a46022ae08b8ded1373
lepkebocs <tkkadartamas@gmail.com>
2016-10-06 10:08:14 +0200

    stuff i made

--- static/base.js
+++ static/base.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
     
 });
+function asd(a)
+{
+    if(a==1)
+    document.getElementById("asd").className = '';
+    else
+    document.getElementById("asd").className = 'hidden';
+}
+/*if(!$('input').val()){
+    $('#register').className = 'hidden';
+}
+else {
+    $('#register').show();
+}*/
+/*function required(inputtx)
+{
+ if (inputtx.value.length == 0)
+  {
+     alert("message");
+     return false;
+  }
+  return true;
+}*/
+(function() {
+    $('form > input').keyup(function() {
+
+        var empty = false;
+        $('form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#register').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#register').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
+})()
