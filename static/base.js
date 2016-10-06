4e42ad5c636aee3f4d54a34b39387b6fadfb7aea
Kristof Galla <gallakristof@gmail.com>
2016-10-07 01:49:31 +0200

    Show new boards when added

--- static/base.js
+++ static/base.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    $("#boards").hide();
     $("#edit").hide();
 
     $("#plus").click(function() {
@@ -13,31 +12,46 @@ $(document).ready(function() {
     });
 
     $("#save").click(function() {
+        // get tile from form (test if filled) and generate code
+        var title = document.getElementById("title").value;
+        if (title === "" || title === null) {
+            return false;
+        }
         $("#edit").hide();
         $("#add").fadeIn("slow");
-        makeNewBoard();
+        // make a new board object
+        var newBoard = {
+            boardId: makeID(),
+            title: title
+        }
+        // save board object to local storage
+        saveBoard(newBoard);
+        // clear form value
+        document.getElementById("title").value = ""
+        var newBoardBox = '<div class="clearfix visible-xs-block"></div>\
+        <div class="col-sm-3 boards" id=' + newBoard["boardId"] + '>\
+        <div class="panel panel-primary">\
+        <div class="panel-heading">' + newBoard["title"] + '</div>\
+        <div class="panel-body"><img src="static/icons/Trello.jpg" class="img-responsive" style="width:100%" alt="Image" id="board"></div>\
+        </div>\
+        </div>'
+        $("#boardRow").prepend(newBoardBox);
+
+        // prepare div
+        // var block = $('<div class="clearfix visible-xs-block"></div>');
+        // var boardDiv = $('<div class="col-sm-3 boards" id=' + newBoard["boardId"] + '></div>');
+        // var panel = $('<div class="panel panel-primary" id=panel' + newBoard["boardId"] + '></div>');
+        // var boardHead = $('<div class="panel-heading"></div>').text(newBoard["title"]);
+        // var boardImage = '<div class="panel-body"><img src="static/icons/Trello.jpg" class="img-responsive" style="width:100%" alt="Image" id="board"></div>';
+        // prepend div of the new board
+        // $("#boardRow").prepend(block);
+        // $("#boardDiv").append(panel);
+        // $("#panel" + newBoard["boardId"]).append(boardHead, boardImage);
     });
 });
 
-var makeNewBoard = function() {
-    // get tile from form (test if filled) and generate code
-    var title = document.getElementById("title").value;
-    if (title === "" || title === null) {
-        return false;
-    }
-    // make a new board object
-    var newBoard = {
-        boardId: "asdf",
-        title: title
-    }
-    // append board object to local storage
-    saveBoard(newBoard);
-    // prepare div
-
-    // prepend div of the new board
-    // clear form value
-    document.getElementById("title").value = ""
-}
+///////////////////
+///////////////////
 
 var saveBoard = function(boardObj) {
     var x = localStorage.getItem('boards');
@@ -47,4 +61,12 @@ var saveBoard = function(boardObj) {
     x = JSON.parse(x);
     x.push(boardObj);
     localStorage.setItem('boards', JSON.stringify(x));
+}
+
+var makeID = function() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i = 0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
 }
