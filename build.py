299c49792d3f2c65cbf63b5c7640a07de9b61153
Kristof Galla <gallakristof@gmail.com>
2016-10-18 13:57:32 +0200

    Update readme with login.txt creation instructions

--- build.py
+++ build.py
@@ -4,8 +4,10 @@ db.connect()
 
 
 def make_tables():
-db.create_tables([Board, Card], safe=True)
+    db.create_tables([Board, Card], safe=True)
 
 def start_over_database():
     db.drop_tables([Board, Card], cascade=True)
     make_tables()
+
+make_tables()
--- models.py
+++ models.py
@@ -3,11 +3,13 @@ from peewee import *
 # everybody should define this on his own machine
 # !!!ADD LOGIN.TXT TO GITIGNORE!!!
 with open('login.txt', 'r') as f:
-    dbname = f.readline().strip()
-db = PostgresqlDatabase(dbname, dbname)
+    login_details = f.readline().strip().split(",")
+    db_name = login_details[0]
+    db_user = login_details[1]
+db = PostgresqlDatabase(db_name, user=db_user)
 
 
-class BaseModel:
+class BaseModel(Model):
 
     class Meta:
         database = db
